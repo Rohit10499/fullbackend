78f8d3fffa034bb62ee651f4bdf0f7b783e4f1bd
Rohit10499 <rohitmaurya10499@gmail.com>
2023-11-24 21:45:08 +0530

    all setup configured

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error {
     statusCoad,
     message = "Something went wrong",
     errors = [],
-    statck = ""
+    stack = ""
   ) {
     super(message);
     this.statusCoad = statusCoad;
